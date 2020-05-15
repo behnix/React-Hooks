3214ed71bdfa9e13e0de832c9164cff68c70b789
Behnam Mahmoodi <behnam.itc@outlook.com>
2020-05-15 15:46:19 +0430

    Use useState hook to add/show songs

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import { v1 as uuidv1 } from "uuid";
 
 const SongList = () => {
-    return ( 
-        <div className="song-list">
-            <ul>
-                <li>Shabe Sard</li>
-                <li>Divar</li>
-            </ul>
-        </div>
-     );
-}
- 
+  const [songs, setSong] = useState([
+    { title: "Rooze Sard", id: 1 },
+    { title: "Divar", id: 2 },
+    { title: "Dahati", id: 3 },
+  ]);
+  const addSong = () => {
+    setSong([...songs, { title: "news song", id: uuidv1() }]);
+  };
+  return (
+    <div className="song-list">
+      <ul>
+        {songs.map((song) => {
+          return <li key={song.id}>{song.title}</li>;
+        })}
+      </ul>
+      <button onClick={addSong}>Add new song</button>
+    </div>
+  );
+};
+
 export default SongList;
