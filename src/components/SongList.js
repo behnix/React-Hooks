71698638b71c2386dcea30d25de3c7a1578afe0a
Behnam Mahmoodi <behnam.itc@outlook.com>
2020-05-15 15:26:40 +0430

    Edit for myself

---
+++ src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SongList = () => {
+    return ( 
+        <div className="song-list">
+            <ul>
+                <li>Shabe Sard</li>
+                <li>Divar</li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default SongList;
